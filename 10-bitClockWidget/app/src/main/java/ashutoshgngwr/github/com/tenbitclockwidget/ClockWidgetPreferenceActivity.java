7cbbc5c43ff48916bb3272271abe7df2ceb92403
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2017-06-15 16:39:25 +0530

    Added ClockWidgetPreferenceActivity as widget configuration activity

--- 10-bitClockWidget/app/src/main/java/ashutoshgngwr/github/com/tenbitclockwidget/ClockWidgetPreferenceActivity.java
+++ 10-bitClockWidget/app/src/main/java/ashutoshgngwr/github/com/tenbitclockwidget/ClockWidgetPreferenceActivity.java
@@ -13,13 +13,16 @@ public class ClockWidgetPreferenceActivity extends AppCompatActivity {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new ClockWidgetPreferenceFragment())
                 .commit();
+
+        // always set result OK because all the widget settings are optional for user to configure.
+        setResult(RESULT_OK, getIntent());
     }
 
     @Override
     protected void onStop() {
         super.onStop();
 
-        // update widget. User might have changed the settings.
+        // send update broadcast to ClockWidgetProvider
         sendBroadcast(new Intent(ClockWidgetProvider.ACTION_UPDATE_CLOCK));
     }
 }
