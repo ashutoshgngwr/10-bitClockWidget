7bead021880fa5297940a550cdd68340700c7939
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2019-05-21 13:30:01 +0530

    optimize memory usage for bitmap rendering

--- 10-bitClockWidget/app/src/main/java/com/github/ashutoshgngwr/tenbitclockwidget/ClockWidgetProvider.java
+++ 10-bitClockWidget/app/src/main/java/com/github/ashutoshgngwr/tenbitclockwidget/ClockWidgetProvider.java
@@ -24,25 +24,14 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.RectF;
 import android.os.Build;
 import android.provider.AlarmClock;
 import android.util.Log;
-import android.util.TypedValue;
 import android.widget.RemoteViews;
-
-import java.util.Calendar;
 
 public class ClockWidgetProvider extends AppWidgetProvider {
 
   private static final String TAG = ClockWidgetProvider.class.getSimpleName();
-
-  private static final int BIT_ALPHA_ACTIVE = 0xFF;
-  private static final int BIT_ALPHA_INACTIVE = 0x80;
-  private static final int SEPARATOR_LINE_ALPHA = 0x70;
 
   private static final int RC_OPEN_CLOCK = 0x12;
 
@@ -68,7 +57,7 @@ public class ClockWidgetProvider extends AppWidgetProvider {
     }
 
     RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.clock_widget_layout);
-    remoteViews.setImageViewBitmap(R.id.iv_clock, renderClockBitmap(context));
+    remoteViews.setImageViewBitmap(R.id.iv_clock, ClockWidgetRenderer.renderBitmap());
     remoteViews.setOnClickPendingIntent(R.id.iv_clock, createOnClickPendingIntent(context));
     appWidgetManager.updateAppWidget(ids, remoteViews);
     Log.d(TAG, "Finished updating widget!");
@@ -104,86 +93,5 @@ public class ClockWidgetProvider extends AppWidgetProvider {
     return PendingIntent.getActivity(context, RC_OPEN_CLOCK,
         context.getPackageManager().getLaunchIntentForPackage(clockInfo.packageName),
         PendingIntent.FLAG_CANCEL_CURRENT);
-  }
-
-  private Bitmap renderClockBitmap(Context context) {
-    // get current time in 12-hour format
-    Calendar c = Calendar.getInstance();
-    int hour = c.get(Calendar.HOUR), minute = c.get(Calendar.MINUTE),
-        am_pm = c.get(Calendar.AM_PM);
-
-    // scale width & height here and all other DIP values will be scaled in px() function
-    int width = Math.round(context.getResources().getDimension(R.dimen.widget_width)),
-        height = Math.round(context.getResources().getDimension(R.dimen.widget_height)),
-        dot_radius = px(ClockWidgetSettings.getDotSize()), // get dot radius as set by user
-        dot_size = dot_radius * 2,
-        // dotSpacingX = spacing between 4 columns + left & right padding
-        dotSpacingX = (width - dot_size * 5 - px(5)) / 6,
-        dotSpacingY = (height - dot_size * 2) / 3; // same as X
-
-    // create a bitmap of widget's size
-    Bitmap clockBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-
-    Canvas canvas = new Canvas(clockBitmap);
-
-    Paint p = new Paint();
-    p.setAntiAlias(true);
-    p.setStyle(Paint.Style.FILL);
-
-    // set clock's background color.
-    p.setColor(ClockWidgetSettings.getClockBackgroundColor());
-    canvas.drawRoundRect(new RectF(0, 0, width - 1, height - 1), px(5), px(5), p);
-
-    // set clock's color based on time.
-    p.setColor(am_pm == Calendar.AM ?
-        ClockWidgetSettings.getClockAMColor() : ClockWidgetSettings.getClockPMColor());
-
-    for (int i = 0; i < 4; i++) {
-      if ((hour >> i & 1) == 1)
-        p.setAlpha(BIT_ALPHA_ACTIVE);
-      else
-        p.setAlpha(BIT_ALPHA_INACTIVE);
-
-      // cx = paddingX + (width + spacing of previous dots) + radius of current dot
-      // cy = paddingY + radius of dot [for line 1],
-      // cy = paddingY + radius of dot + height of line 1 + dotSpacingY [for line 2]
-      canvas.drawCircle(dotSpacingX + (1 - i % 2) * (dot_size + dotSpacingX) + dot_radius,
-          dotSpacingY + dot_radius + (i / 2 == 0 ? dotSpacingY + dot_size : 0),
-          dot_radius, p);
-    }
-
-    // cx = padding + width of 2 dots + spacing of 2 dots + 5dp (separator padding + width)
-    int marginX = dotSpacingX + (dot_size + dotSpacingX) * 2 + px(5);
-
-    for (int i = 0; i < 6; i++) {
-      if ((minute >> i & 1) == 1)
-        p.setAlpha(BIT_ALPHA_ACTIVE);
-      else
-        p.setAlpha(BIT_ALPHA_INACTIVE);
-
-      // cx = marginX + (width + spacing of previous dots) + radius of current dot.
-      // cy = paddingY + radius of dot [for line 1]
-      // cy = paddingY + radius of dot + height of line 1 + dotSpacingY [for line 2]
-      canvas.drawCircle(marginX + (2 - i % 3) * (dot_size + dotSpacingX) + dot_radius,
-          dotSpacingY + dot_radius + (i / 3 == 0 ? dotSpacingY + dot_size : 0),
-          dot_radius, p);
-    }
-
-    if (ClockWidgetSettings.shouldDisplaySeparator()) {
-      float x1 = dotSpacingX + dot_size * 2 + dotSpacingX * 1.5F + px(2);
-
-      p.setAlpha(SEPARATOR_LINE_ALPHA);
-
-      // from center-axis of line 1's dot to center-axis of line 2's dot
-      canvas.drawLine(x1, dotSpacingY + dot_radius, x1,
-          dotSpacingY + dot_radius + dotSpacingY + dot_size, p);
-    }
-
-    return clockBitmap;
-  }
-
-  private int px(int dp) {
-    return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
-        ClockWidgetApplication.getContext().getResources().getDisplayMetrics()));
   }
 }
